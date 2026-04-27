c6335f8531a6ea92f25801ab0a9315ceddee9b7d
Daniel King <dking9486@gmail.com>
2026-04-27 18:21:41 +0100

    sidebar mobile

--- src/features/Dashboard/Sidebar.jsx
+++ src/features/Dashboard/Sidebar.jsx
@@ -2,11 +2,15 @@ import { MdOutlineDashboard } from "react-icons/md";
 import { BsPerson } from "react-icons/bs";
 import { GoVerified, GoSignOut } from "react-icons/go";
 import { BsCart2 } from "react-icons/bs";
-function Sidebar() {
+import { HiXMark } from "react-icons/hi2";
+function Sidebar({isOpen, setIsOpen}) {
     return (
-        <aside className=" hidden flex-1 bg-primary-light px-3 py-6 lg:flex flex-col gap-7 h-screen">
-            <picture className="px-3">
+        <aside className={`fixed inset-y-0 left-0 z-30 w-64 transform p-4 transition-transform duration-300 ease-in-out lg:relative lg:translate-x-0 lg:flex-1 bg-primary-light px-3 py-6 lg:flex flex-col gap-7 h-screen ${isOpen ? "flex translate-x-0" : "-translate-x-full"}`}>
+            <picture className="flex items-center px-3">
                 <img src="../logo-white.svg" alt="logo" className="" />
+                <button className="lg:hidden group cursor-pointer ml-auto">
+                    <HiXMark className="text-lg lg:text-xl font-bold text-white" onClick={setIsOpen} />
+                </button>
             </picture>
             <div className="px-4 space-y-3">
                 <div className="flex items-center gap-3 px-3 py-2 rounded-lg text-white hover:bg-white hover:text-primary transition ease-in-out duration-200 cursor-pointer">
--- src/layouts/DashLayout.jsx
+++ src/layouts/DashLayout.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react"
 import NavbarDash from "../features/Dashboard/NavbarDash"
 import Sidebar from "../features/Dashboard/Sidebar"
 
 function DashLayout({children}) {
+    const [isOpen, setIsOpen] = useState(false);
+    // const handleNav = ()=>{
+    //     setIsOpen(true);
+    // }
     return (
         <div className="flex">
-            <Sidebar />
+            <div className={`fixed inset-0 z-20 bg-black/50 backdrop-blur-sm transition-opacity lg:relative ${isOpen ? "opacity-100" : "opacity-0 lg:opacity-100 pointer-events-none lg:pointer-events-auto "}`}>
+                <Sidebar isOpen={isOpen} setIsOpen={()=> setIsOpen(false)} />
+            </div>
             <div className="flex-6">
                 <nav className="bg-white px-3 py-2 lg:px-12 lg:py-3">
-                    <NavbarDash />
+                    <NavbarDash setIsOpen={()=> setIsOpen(true)} />
                 </nav>
                 {children}
             </div>

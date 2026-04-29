c84854b1734c298803fdfb3c713eb44eb2964dab
Daniel King <dking9486@gmail.com>
2026-04-29 18:32:01 +0100

    allSellers page

---
+++ src/Pages/public/AllSellers.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { GoHome } from "react-icons/go";
+import SplashScreen from "../../ui/SplashScreen";
+import useCategories from "../../features/categories/useCategories";
+import useSearchSeller from "../../features/profiles/useSearchSeller";
+// import SearchBar from "../../ui/SearchBar";
+import NetworkError from "../../ui/NetworkError";
+import MainLayout from "../../layouts/MainLayout";
+import OtherCategoriesList from "../../ui/OtherCategoriesList";
+import useSeller from "../../features/profiles/useSeller";
+import SellersBusinessList from "../../ui/SellersBusinessList";
+
+
+
+export default function CategorySellers() {
+  const { loading: searchLoading, error: searchError, sellers: searchSellers, searchSellers: performSearch } = useSearchSeller();
+  const { loading: categoriesLoading, categories, getAllCategories } = useCategories();
+  const {loading: sellersLoading, sellers, fetchAllSellers} = useSeller();
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+        const fetchData = async ()=> {
+            try{
+                fetchAllSellers();
+                getAllCategories();
+            }
+            catch(err){
+                console.log("Batch Failed: ", err);
+            }
+        }
+        fetchData();
+    }, []);
+
+  const handleSearch = (searchQuery) => {
+    setQuery(searchQuery);
+    performSearch(searchQuery);
+  };
+
+  
+
+  // Show loading spinner while fetching categories or sellers
+  if (sellersLoading || categoriesLoading) return <SplashScreen />;
+  return (
+    <MainLayout>
+          <main className="space-y-8 px-4 py-2 lg:px-12 lg:py-3">
+            <div className="flex items-center">
+              <Link to="/">
+                <GoHome className="text-lg lg:text-xl hover:text-primary-light" />
+              </Link>
+              <h1 className="capitalize text-xl lg:text-3xl font-medium">/Sellers</h1>
+            </div>
+            <OtherCategoriesList categories={categories} />
+            <SellersBusinessList />
+          </main>
+    </MainLayout>
+  );
+}
--- src/features/profiles/useSeller.js
+++ src/features/profiles/useSeller.js
@@ -1,10 +1,11 @@
 import { useCallback, useState } from "react";
-import { getSellerById, getSellerByUsername } from "../../services/apiSellers";
+import { getAllActiveSellers, getSellerById, getSellerByUsername } from "../../services/apiSellers";
 
 function useSeller(){
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [seller, setSeller] = useState(null);
+    const [sellers, setSellers] = useState(null);
 
     const fetchSellerById = useCallback(async (id) => {
         setLoading(true);
@@ -38,8 +39,25 @@ function useSeller(){
         }
     }, []);
 
+    const fetchAllSellers = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+        try{
+            const data = await getAllActiveSellers();
+            setSellers(data);
+        }
+        catch(err){
+            console.log(err)
+            setError(err.message);
+            throw new Error(err);
+        }
+        finally {
+            setLoading(false);
+        }
+    }, []);
+
     
-    return {loading, error, seller, fetchSellerById, fetchSellerByUsername};
+    return {loading, error, seller, fetchSellerById, fetchSellerByUsername, sellers, fetchAllSellers};
 }
 
 

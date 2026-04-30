f622ba0d1b420e5e8b0f660624d9fa8c5c15a78e
Daniel King <dking9486@gmail.com>
2026-04-30 06:47:11 +0100

    users migration

--- src/Pages/public/AllSellers.jsx
+++ src/Pages/public/AllSellers.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { GoHome } from "react-icons/go";
 import SplashScreen from "../../ui/SplashScreen";
 import useCategories from "../../features/categories/useCategories";
@@ -15,7 +15,7 @@ import SellersBusinessList from "../../ui/SellersBusinessList";
 
 export default function CategorySellers() {
   const { loading: searchLoading, error: searchError, sellers: searchSellers, searchSellers: performSearch } = useSearchSeller();
-  const { loading: categoriesLoading, categories, getAllCategories } = useCategories();
+  const { loading: categoriesLoading, categories, getAllCategories, error: categoryError } = useCategories();
   const {loading: sellersLoading, sellers, fetchAllSellers} = useSeller();
   const [query, setQuery] = useState("");
 
@@ -41,6 +41,7 @@ export default function CategorySellers() {
 
   // Show loading spinner while fetching categories or sellers
   if (sellersLoading || categoriesLoading) return <SplashScreen />;
+  if (categoryError || searchError) return <NetworkError />;
   return (
     <MainLayout>
           <main className="space-y-8 px-4 py-2 lg:px-12 lg:py-3">
